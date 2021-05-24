b002ac0457732258876b0a2a38c2ad7eeb98acf4
iadirock <aditya16219@iiitd.ac.in>
2021-05-24 16:34:37 +0530

    Add the text and buttons on screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,44 @@
 import 'package:flutter/material.dart';
 
 //main function which runs automatically on starting the app
-void main() {
-  //Instantiatng my MyApp class
-  runApp(MyApp());
-}
+
+//Instantiatng my MyApp class
+void main() => runApp(MyApp());
+//shortcut for braces '{}' = '=>'. Only use when function have only 1 expression
 
 //statelesswidget is provided by the material.dart
 class MyApp extends StatelessWidget {
+  // void answerFunc() {
+  // print('answer chosen');
+  // }
+
+  //decorator provided by flutter and dart
+  //Basically we are overriding the build function provided by the stateless widget
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Helloo!'));
+    // scaffold gives you a basic design
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: () => print('Answer 1'),
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2'),
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () => print('Answer 3'),
+              ),
+            ],
+          )),
+    );
   }
 }
